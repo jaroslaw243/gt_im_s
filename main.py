3e16cf1c7fac03019c03ae77672b0bd5c353f417
jaroslaw243 <boss.mielczarek@gmail.com>
2020-11-13 21:27:30 +0100

    test image generated

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('test_circle.png', 0)
+
+gaussian_noise = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
+cv2.randn(gaussian_noise, 100, 20)
+img_noise = img + gaussian_noise
+
+plt.imshow(img_noise, cmap='gray')
+plt.xticks([]), plt.yticks([])
+plt.show()

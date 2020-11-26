16a74910a919c8d956ed254cb21ea1774b4d90b1
jaroslaw243 <boss.mielczarek@gmail.com>
2020-11-26 01:40:43 +0100

    testing region descend methods

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import cv2
-import numpy as np
+# import numpy as np
+import autograd.numpy as np
+from autograd import grad
 from matplotlib import pyplot as plt
 import copy
 
 
-def region_segmentation_cost(image, segmentation, constant):
+def region_segmentation_cost(segmentation):
+    constant = 2
+
+    img = cv2.imread('test_circle.png', 0)
+
+    gaussian_noise = np.random.normal(0, 25, size=(img.shape[0], img.shape[1]))
+
+    img_noise_temp = np.array(img, dtype=np.int32) + gaussian_noise
+    img_noise_temp[img_noise_temp > 255] = 255
+    img_noise_temp[img_noise_temp < 0] = 0
+    image = np.array(img_noise_temp, dtype=np.uint8)
+
     data_fidelity_term = np.sum(np.square(image - segmentation))
     smoothness_term = 0
-    segmentation = np.int8(np.pad(segmentation, 1, 'edge'))
+    segmentation = np.pad(segmentation, 1, 'constant')
     for i in range(1, segmentation.shape[0] - 2):
         for j in range(1, segmentation.shape[1] - 2):
             smoothness_term += (np.square(segmentation[i][j] - segmentation[i - 1][j]) + np.square(segmentation[i][j] - segmentation[i][j + 1]))
@@ -50,20 +63,31 @@ img_contour2 = copy.copy(img_noise)
 cv2.drawContours(img_contour2, contours2, -1, 255, 3)
 
 ret, best_img_seg = cv2.threshold(img_noise, 100, 1, cv2.THRESH_BINARY)
-min_cost = region_segmentation_cost(img_noise, best_img_seg, 2)
+# min_cost = region_segmentation_cost(img_noise, best_img_seg, 2)
+# worst_img_seg = best_img_seg
+# max_cost = min_cost
+# for tr in range(140, 210, 10):
+#     ret, img_seg = cv2.threshold(img_noise, tr, 1, cv2.THRESH_BINARY)
+#     cost = region_segmentation_cost(img_noise, img_seg, 2)
+#
+#     if cost < min_cost:
+#         min_cost = cost
+#         best_img_seg = img_seg
+#
+#     if cost > max_cost:
+#         max_cost = cost
+#         worst_img_seg = img_seg
+
+min_cost = region_segmentation_cost(best_img_seg)
+super_jajo = grad(region_segmentation_cost)
 worst_img_seg = best_img_seg
 max_cost = min_cost
 for tr in range(140, 210, 10):
     ret, img_seg = cv2.threshold(img_noise, tr, 1, cv2.THRESH_BINARY)
-    cost = region_segmentation_cost(img_noise, img_seg, 2)
+    best_img_seg -= super_jajo(np.array(img_seg, dtype=np.float))
 
-    if cost < min_cost:
-        min_cost = cost
-        best_img_seg = img_seg
+print(best_img_seg)
 
-    if cost > max_cost:
-        max_cost = cost
-        worst_img_seg = img_seg
 
 fig, ax = plt.subplots(1, 3)
 plt.setp(ax, xticks=[], yticks=[])

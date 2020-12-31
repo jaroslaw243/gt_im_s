fixed boundary cost calculation

main.py
@@ -63,19 +63,20 @@ def boundary_segmentation_cost(image, contour):
 
 def boundary_segmentation_cost_interlaced(image, contour, segmentation, beta):
     global full_b_cost
-    b_cost = 0
 
-    for i in range(len(contour[0])):
-        x = contour[0][i][0][0]
-        y = contour[0][i][0][1]
-        b_cost += image[x, y]
+    drawn_contour = cv2.drawContours(np.zeros(image.shape, dtype=np.int32), contour, -1, 1, 1)
+    b_cost = np.sum(image[drawn_contour == 1])
 
     full_b_cost += b_cost
 
     contour_matrix = np.zeros(segmentation.shape, dtype=np.int32)
     contour_matrix = cv2.drawContours(contour_matrix, contour, -1, 1, -1)
 
-    region_module_influence = np.sum(segmentation[contour_matrix == 1])
+    image_r = copy.copy(segmentation)
+    image_r = np.array(image_r, dtype=np.int8)
+    image_r[image_r == 0] = -10
+
+    region_module_influence = np.sum(image_r[contour_matrix == 1])
 
     return b_cost + (beta * region_module_influence)
 
@@ -193,9 +194,9 @@ def boundary_finding_interlaced(coefficients, *args):
     return -boundary_segmentation_cost_interlaced(args[0], current_contour, args[3], args[4])
 
 
-img = cv2.imread('test_complex.png', 0)
+img = cv2.imread('test_complex2.png', 0)
 
-gaussian_noise = np.random.normal(0, 150, size=(img.shape[0], img.shape[1]))
+gaussian_noise = np.random.normal(0, 0, size=img.shape)
 
 img_noise_temp = np.array(img, dtype=np.int32) + gaussian_noise
 img_noise_temp[img_noise_temp > 255] = 255
@@ -218,15 +219,15 @@ img_contour = copy.copy(img)
 cv2.drawContours(img_contour,
                  reconstructed_contour_to_opencv_contour(reconstruct_contour(locus=init_fourier_coeffs_first_part,
                                                                              coeffs=init_fourier_coeffs_second_part,
-                                                                             num_points=10000)), -1, 0, 1)
+                                                                             num_points=2000)), -1, 0, 1)
 
 init_tr = 180
 clique_size = 1
-sm_const = 15
-scaling_const_alpha = 500
-scaling_const_beta = 2500
+sm_const = 20
+scaling_const_alpha = 0
+scaling_const_beta = 1.0
 max_iterations = 10
-p2c_acc = 10000
+p2c_acc = 2000
 ret, init_img_seg = cv2.threshold(img_noise, init_tr, 1, cv2.THRESH_BINARY)
 
 start_time_region = time.time()
@@ -237,11 +238,13 @@ region_segmentation2 = iterated_conditional_modes_interlaced(img_noise, init_img
 
 final_time_region = time.time() - start_time_region
 
-full_b_cost = boundary_segmentation_cost(img_gradient,
-                                         reconstructed_contour_to_opencv_contour(
-                                             reconstruct_contour(locus=init_fourier_coeffs_first_part,
-                                                                 coeffs=init_fourier_coeffs_second_part,
-                                                                 num_points=p2c_acc)))
+full_b_cost = 0
+full_b_cost = boundary_segmentation_cost_interlaced(img_gradient,
+                                                    reconstructed_contour_to_opencv_contour(
+                                                        reconstruct_contour(locus=init_fourier_coeffs_first_part,
+                                                                            coeffs=init_fourier_coeffs_second_part,
+                                                                            num_points=p2c_acc)), region_segmentation2,
+                                                    scaling_const_beta)
 
 boundary_cost = boundary_segmentation_cost_interlaced(img_gradient, contours, region_segmentation2, scaling_const_beta)
 
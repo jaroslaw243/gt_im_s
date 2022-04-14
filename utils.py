2a532ba4880d2c3503caf5de9e264b99e8418f7f
jaroslaw243 <boss.mielczarek@gmail.com>
2022-04-14 19:51:03 +0200

    added function for removing vignette effects from binarized image

--- utils.py
+++ utils.py
@@ -36,3 +36,21 @@ def hair_removal(image):
 def image_list_ph2(beginning_of_the_path, first_image=1, last_image=25):
     return [beginning_of_the_path + r'\IMD%03d\IMD%03d_Dermoscopic_Image\IMD%03d.bmp' % (i, i, i) for i in
             range(first_image, last_image + 1)]
+
+
+def fill_image_seg_boundaries(seg):
+    org_seg = copy.copy(seg)
+    inv_seg = cv2.bitwise_not(org_seg)
+
+    filled_cor1 = cv2.floodFill(inv_seg, None, (0, 0), 1)
+    filled_cor2 = cv2.floodFill(inv_seg, None, (inv_seg.shape[1] - 1, 0), 1)
+    filled_cor3 = cv2.floodFill(inv_seg, None, (0, inv_seg.shape[0] - 1), 1)
+    filled_cor4 = cv2.floodFill(inv_seg, None, (inv_seg.shape[1] - 1, inv_seg.shape[0] - 1), 1)
+
+    comb1 = filled_cor1[1] & filled_cor2[1]
+    comb2 = comb1 & filled_cor3[1]
+    comb3 = comb2 & filled_cor4[1]
+
+    org_seg[comb3 == 1] = 0
+
+    return org_seg
